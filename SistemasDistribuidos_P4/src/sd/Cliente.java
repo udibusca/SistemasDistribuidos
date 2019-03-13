6e676d71fd79deb928313f935a2673f306d5c014
André Luiz de Castro Alves <alpmidia@gmail.com>
2019-03-13 13:55:03 -0300

    Add files via upload

--- SistemasDistribuidos_P4/src/sd/Cliente.java
+++ SistemasDistribuidos_P4/src/sd/Cliente.java
@@ -1,105 +1,30 @@
 package sd;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-
-public class Cliente extends Thread {
-	/**
-	 * parte que controla a recepção de mensagens do cliente
-	 **/
-	private Socket conexao;
-
-	/**
-	 * construtor que recebe o socket do cliente
-	 **/
-	public Cliente(Socket socket) {
-		this.conexao = socket;
-	}
+public class Cliente {
 
 	public static void main(String args[]) {
+
 		try {
-			/**
-			 * Instancia do atributo conexao do tipo Socket, conecta a IP do Servidor, Porta
-			 */
-			Socket socket = new Socket("127.0.0.1", 5555);
-
-			/**
-			 * Instancia do atributo saida, obtem os objetos que permitem controlar o fluxo
-			 * de comunicação
-			 */
-			PrintStream saida = new PrintStream(socket.getOutputStream());
-			BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Digite seu nome: ");
-			String meuNome = teclado.readLine();
-
-			/**
-			 * envia o nome digitado para o servidor
-			 */
-			saida.println(meuNome.toUpperCase());
-			/**
-			 * instancia a thread para ip e porta conectados e depois inicia ela
-			 */
-			Thread thread = new Cliente(socket);
-			thread.start();
-			/**
-			 * Cria a variavel msg responsavel por enviar a mensagem para o servidor
-			 */
-			String msg;
-			while (true) {
-				/**
-				 * cria linha para digitação da mensagem e a armazena na variavel msg
-				 */
-				System.out.print("Mensagem > ");
-				msg = teclado.readLine();
-				/**
-				 * envia a mensagem para o servidor
-				 */
-				saida.println(msg);
-			}
-		} catch (IOException e) {
-			System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
-		}
-	}
-
-	public void run() {
-		try {
-			/**
-			 * recebe mensagens de outro cliente através do servidor
-			 */
-			BufferedReader entrada = new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
-
-			String mensagem;
-			while (true) {
-				/**
-				 * pega o que o servidor enviou
-				 */
-				mensagem = entrada.readLine();
-				/**
-				 * se a mensagem contiver dados, passa pelo if, caso contrario cai no break e
-				 * encerra a conexao
-				 */
-				if (mensagem == null) {
-					System.out.println("Conexão encerrada!");
-					System.exit(0);
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Andre".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
 				}
-				System.out.println();
-				/**
-				 * imprime a mensagem recebida
-				 */
-				System.out.println(mensagem);
-				/**
-				 * cria uma linha visual para resposta
-				 */
-				System.out.print("Responder > ");
 			}
-		} catch (IOException e) {
-			/**
-			 * caso ocorra alguma exceção de E/S, mostra qual foi.
-			 */
-			System.out.println("Ocorreu uma Falha... .. ." + " IOException: " + e);
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(ClienteFrame_old.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(ClienteFrame_old.class.getName()).log(java.util.logging.Level.SEVERE, null,ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(ClienteFrame_old.class.getName()).log(java.util.logging.Level.SEVERE, null,ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(ClienteFrame_old.class.getName()).log(java.util.logging.Level.SEVERE, null,ex);
 		}
+
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new ClienteFrame_old().setVisible(true);
+			}
+		});
 	}
 }
